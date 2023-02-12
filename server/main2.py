330857dd5eb1285f0f6aa8efba600c654f2b184f
nicolas carrara <nicolas.carrara@myrill.io>
2023-02-12 23:10:05 +0100

    cleaning

--- server/main2.py
+++
@@ -1,7 +0,0 @@
-import mdtex2html
-
-with open("../data/1.md") as f:
-
-    x  = mdtex2html.convert(f.read())
-    with open("1.html", "w") as g:
-        g.write(x)
--- server/main3.py
+++
@@ -1,5 +0,0 @@
-import markdown
-
-md = markdown.Markdown(extensions=['mdx_math'])
-with open("3.html", "w") as g:
-    g.write(md.convert('$$e^x$$'))
